275a8432fe45a29be3f2471679ef7b6efab0f76b
Yong Hao TENG <yonghao.teng@gmail.com>
2019-01-21 14:24:59 +0800

    Add scripts

---
+++ get-reposense.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import os
+import requests
+import subprocess
+
+JAR_FILENAME = 'RepoSense.jar'
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Downloads a specific version of RepoSense.jar from our repository.')
+    
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-r', '--release', action='store_true', help='Get RepoSense.jar from the latest release (Stable)')
+    group.add_argument('-m', '--master', action='store_true', help='Get RepoSense.jar from the latest master (Beta)')
+    group.add_argument('-t', '--tag', help='Get RepoSense.jar of a specific release version tag')
+
+    parser.add_argument('-o', '--overwrite', action='store_true', help='Overwrite RepoSense.jar file, if exists. Default: false')
+
+    return parser.parse_args()
+
+def handle_specific_release(tag):
+    get_reposense_jar('https://api.github.com/repos/reposense/RepoSense/releases/tags/' + tag, tag)
+
+def handle_latest_release():
+    get_reposense_jar('https://api.github.com/repos/reposense/RepoSense/releases/latest')
+
+def get_reposense_jar(url, tag=None):
+    response = requests.get(url)
+
+    if tag and response.status_code == 404:
+        print('Error, tag does not exists!')
+        exit(1)
+
+    if response.status_code == 403:
+        print('GitHub API has exceed the rate limit. Falling back to alternative method...')
+        clone_and_make_reposense(tag)
+        return
+
+    url = response.json()['assets'][0]['browser_download_url']
+    download_file(url)
+
+def clone_and_make_reposense(tag=None):
+    
+    command = \
+    '''
+    git clone 'https://github.com/reposense/RepoSense.git' &&
+    cd RepoSense &&
+    '''
+
+    if tag:
+        command += 'git checkout tags/{} -b deploy &&'.format(tag)
+
+    command += \
+    '''
+    ./gradlew zipreport shadowjar &&
+    mv build/jar/RepoSense.jar ../
+    '''
+   
+    subprocess.check_call(command, shell=True)
+
+def download_file(url):
+    response = requests.get(url, allow_redirects=True)
+    open(JAR_FILENAME, 'wb').write(response.content)
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    if os.path.exists(JAR_FILENAME) and args.overwrite is False:
+        print(JAR_FILENAME + ' already exists. Quitting.')
+        exit()
+
+    if args.tag:
+        handle_specific_release(args.tag)
+        exit()
+    
+    if args.master:
+        clone_and_make_reposense()
+        exit()
+
+    # If no arguments are provided or --release
+    handle_latest_release()
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Downloads a specific version of RepoSense.jar of your choice from our repository
+## Examples of supported options:
+### ./get-reposense.py --release               # Gets the latest release (Stable)
+### ./get-reposense.py --master                # Gets the latest master  (Beta)
+### ./get-reposense.py --tag v1.6.1            # Gets a specific version
+### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
+
+./get-reposense.py --release
+
+# Executes RepoSense
+# Do not change the default output folder name (reposense-report)
+## Examples of other valid options; For more, please view the user guide
+### java -jar RepoSense.jar -repos https://github.com/reposense/RepoSense.git
+
+java -jar RepoSense.jar -config ./configs
