db372aea76d344a2ff03e60a9c461dc75e5a0b75
Damith C. Rajapakse <damith@gmail.com>
2022-09-25 01:23:23 +0800

    Add configs

--- run.sh
+++ run.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
 
-# Downloads a specific version of RepoSense.jar of your choice from our repository
-## Examples of supported options:
-### ./get-reposense.py --release               # Gets the latest release (Stable)
-### ./get-reposense.py --master                # Gets the latest master (Beta)
-### ./get-reposense.py --tag v1.6.1            # Gets a specific version
-### ./get-reposense.py --commit abc123         # Gets a specific commit
-### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
-
-./get-reposense.py --release
-
-# Executes RepoSense
-# Do not change the default output folder name (reposense-report)
-## Examples of other valid options; For more, please view the user guide
-### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
-
-java -jar RepoSense.jar --config ./configs
+./get-reposense.py --master
+java -jar RepoSense.jar --config ./configs --since 16/09/2022  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
